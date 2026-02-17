d4bd7a4991948fbf78e89209a5a61384078906a7
Descent Chapfuwa <descenttf@gmail.com>
2026-02-17 20:43:06 +0200

    In the process of making the about section

---
+++ src/components/About.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { productsList } from "../constants";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useMediaQuery } from "react-responsive";
+
+
+const About = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useGSAP(() => {
+    const start = isMobile ? "top 20% " : "top top";
+
+    const maskTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#design",
+        start: start,
+        end: "bottom center",
+        scrub: 1.5,
+        pin: true,
+      },
+    });
+
+    maskTimeline
+      .to(".will-fade", { opacity: 0, stagger: 0.2, ease: "power1.inOut" })
+      .to(".masked-img", {
+        scale: 1.3,
+        maskPosition: "center",
+        maskSize: "400%",
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to("#masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
+  });
+
+  return (
+    <div id="about">
+      <div className="container mx-auto h-full pt-20">
+        <h2 className="will-fade">AB <span></span>OUT</h2>
+
+        <div className="content">
+          <ul className="space-y-4 will-fade">
+            {productsList.map((graphics, index) => (
+              <li key={index} className="flex items-center gap-2">
+                <img src="/images/check.png" alt="check" />
+                <p>{graphics.title}</p>
+              </li>
+            ))}
+          </ul>
+
+          <div className="cocktail-img">
+            <img
+              src="/images/agri-transparent.png"
+              alt="cocktail"
+              className="masked-img w-full h-full object-contain"
+            />
+          </div>
+
+          <ul className="space-y-4 will-fade">
+            {productsList.map((graphics, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-start  gap-2"
+              >
+               <img src="/images/check.png" alt="check" />
+                <p className="md:w-fit w-60 ">{graphics.title}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="masked-container">
+          <h2 className="will-fade">Stareworthy Perfection</h2>
+          <div id="masked-content">
+            <h3>Made with craft ,Edited with passion</h3>
+            <p>
+              It wont be just an image,video or poster after we are done. It
+              will emerge as a carefully crafted moment made just for you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
